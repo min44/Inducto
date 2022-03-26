e63e6d9b30ef9aa4d57597904c6dbc11b6e14ff7
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-03-26 22:14:33 +0700

    Initial commit.

---
+++ Inducto.Startup/Program.fs
@@ -0,0 +1,29 @@
+module Inducto.Library.Main
+
+open Inducto.Startup.Reflection
+
+type Gender =
+    | Binary
+    | NonBinary
+
+type Employee(name, age, salary, gender) =
+    member x.Name = name
+    member x.Age = age
+    member x.Salary = salary
+    member x.Gender = GetCaseByName<Gender> gender
+    override x.ToString() = $"{x.Name} {x.Age} {x.Salary} {x.Gender}"
+    member x.Redundant sal =
+        if sal > 5000.0
+        then printfn "good boy"
+        else printfn "u fired"
+
+type Driver(name, age, salary, gender) =
+    inherit Employee(name, age, salary, gender)
+    member x.LicenceNumber = 0
+
+let hobo = Employee("Bob", 45, 1500.0, "NonBinary")
+let hoboDriver = Driver(hobo.Name,hobo.Age, hobo.Salary, hobo.Gender.ToString())
+let hoboEmployee = hoboDriver :> Employee
+let employeeList = [ hobo; hoboEmployee ]
+employeeList|> Seq.iter (fun x -> printf $"{x.Name}" )
+
---
+++ Inducto.Startup/Reflection.fs
@@ -0,0 +1,29 @@
+﻿module Inducto.Startup.Reflection
+
+open System
+open Microsoft.FSharp.Reflection
+
+module private Reflection =
+    let GetCaseInfos<'T1> = FSharpType.GetUnionCases(typeof<'T1>)
+
+    let MakeCase<'T1> (caseInfo: UnionCaseInfo) = FSharpValue.MakeUnion(caseInfo, [||]) :?> 'T1
+
+    let rec GetAllCaseObjects t = [|
+        for unionCaseInfo in FSharpType.GetUnionCases t do
+            match unionCaseInfo.GetFields() with
+            | [|propertyInfo|] when FSharpType.IsUnion propertyInfo.PropertyType ->
+                for x in GetAllCaseObjects propertyInfo.PropertyType -> FSharpValue.MakeUnion(unionCaseInfo, [|x|])
+            | _ -> yield FSharpValue.MakeUnion(unionCaseInfo, [||]) |]
+   
+open Reflection
+let GetAllCases<'T1> = GetAllCaseObjects typeof<'T1> |> Seq.cast<'T1> |> List.ofSeq
+    
+let GetCaseByName<'x> name =
+    try 
+        let allCases = GetAllCases<'x>
+        let case = allCases |> Seq.tryFind (fun x -> x.ToString() = name)
+        match case with
+        | Some valueCase -> valueCase
+        | None -> raise <| Exception $"Error: Case name {name} is not exist in {typeof<'x>}"
+    with ex -> raise <| Exception ex.Message
+    
