3905fd6e3924d9bda87de12c0ff98e25f610ef67
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-03-27 16:53:59 +0700

    Person generator.

---
+++ Inducto.Startup/Library.fs
@@ -0,0 +1,23 @@
+﻿module Inducto.Startup.Library
+
+open System
+
+let someList = [ 1; 2; 3; 4; 5 ]
+
+let mapResult = someList |> Seq.map (fun x -> x * 2)
+
+let filterResult = someList |> Seq.filter (fun x -> if x < 2 then failwith "Error" else x % 2 <> 0)
+
+let iterResult = someList |> Seq.iter (fun x -> printf $"{x}")
+
+let someResult = someList |> Seq.append [ 2; 3; 4; 5; 6 ]
+
+someResult |> Seq.iter(fun x -> printfn $"Element: {x}")
+
+let GetRandom() = Random().Next(10000, 99999).ToString()
+let GetGuid() = Guid.NewGuid()
+
+let someRandomStringList = [0..999] |> Seq.map(fun _ -> GetGuid())
+
+
+someRandomStringList |> Seq.iter(fun x -> printfn $"String: {x}") 
--- Inducto.Startup/Program.fs
+++ Inducto.Startup/Program.fs
@@ -1,29 +1,119 @@
 module Inducto.Library.Main
 
-open Inducto.Startup.Reflection
+open System
 
 type Gender =
-    | Binary
-    | NonBinary
+    | Male
+    | Female
 
-type Employee(name, age, salary, gender) =
+let Names =
+    [ "James"
+      "Robert"
+      "John"
+      "Michael"
+      "William"
+      "David"
+      "Richard"
+      "Joseph"
+      "Thomas"
+      "Charles"
+      "Christopher"
+      "Daniel"
+      "Matthew"
+      "Anthony"
+      "Jeffrey"
+      "Ryan"
+      "Jacob"
+      "Gary"
+      "Nicholas"
+      "Eric"
+      "Jonathan"
+      "Stephen"
+      "Larry"
+      "Justin"
+      "Scott"
+      "Brandon"
+      "Samuel"
+      "Gregory"
+      "Frank"
+      "Alexander"
+      "Raymond"
+      "Patrick"
+      "Jack"
+      "Dennis"
+      "Jerry"
+      "Tyler"
+      "Aaron"
+      "Jose"
+      "Adam"
+      "Henry"
+      "Nathan"
+      "Douglas"
+      "Zachary"
+      "Peter"
+      "Kyle"
+      "Walter"
+      "Ethan"
+      "Jeremy"
+      "Harold"
+      "Keith"
+      "Christian"
+      "Roger"
+      "Noah"
+      "Gerald"
+      "Carl"
+      "Terry"
+      "Sean"
+      "Austin"
+      "Arthur"
+      "Lawrence"
+      "Jesse"
+      "Dylan"
+      "Bryan"
+      "Joe"
+      "Jordan"
+      "Billy"
+      "Bruce"
+      "Albert"
+      "Willie"
+      "Gabriel"
+      "Logan"
+      "Alan"
+      "Juan"
+      "Wayne"
+      "Roy"
+      "Ralph"
+      "Randy"
+      "Eugene"
+      "Vincent"
+      "Russell"
+      "Elijah"
+      "Louis"
+      "Bobby"
+      "Philip"
+      "Johnny" ]
+let GetRandom x y = Random().Next(x, y)
+let GetRandomName () = Names |> List.item (GetRandom 0 (Names.Length - 1))
+let GetRandomAge () = GetRandom 18 60
+let GetRandomSalary () = float <| GetRandom 10000 45000 
+let GetRandomGender () = if GetRandom 0 100 < 50 then Female else Male
+
+type Employee(id, name, age, salary, gender) =
+    member x.Id = id
     member x.Name = name
     member x.Age = age
     member x.Salary = salary
-    member x.Gender = GetCaseByName<Gender> gender
+    member x.Gender = gender
     override x.ToString() = $"{x.Name} {x.Age} {x.Salary} {x.Gender}"
-    member x.Redundant sal =
-        if sal > 5000.0
-        then printfn "good boy"
-        else printfn "u fired"
+    member x.Redundant sal = if sal > 5000.0 then printfn "good boy" else printfn "u fired"
 
-type Driver(name, age, salary, gender) =
-    inherit Employee(name, age, salary, gender)
+type Driver(id, name, age, salary, gender) =
+    inherit Employee(id, name, age, salary, gender)
     member x.LicenceNumber = 0
 
-let hobo = Employee("Bob", 45, 1500.0, "NonBinary")
-let hoboDriver = Driver(hobo.Name,hobo.Age, hobo.Salary, hobo.Gender.ToString())
-let hoboEmployee = hoboDriver :> Employee
-let employeeList = [ hobo; hoboEmployee ]
-employeeList|> Seq.iter (fun x -> printf $"{x.Name}" )
+let Employees = [ for n in 0 .. 1000 -> Employee(n, GetRandomName(), GetRandomAge(), GetRandomSalary(), GetRandomGender()) ]
+
+printfn "\n\n PERSONS:\n"
+Employees |> Seq.iter(fun i -> printfn $"{i.ToString()}" )
+
 
