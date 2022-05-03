9d49e20d17497347a4d5845f83f3b9271d2d1e00
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-05-03 18:47:26 +0700

    Big knowledge added

--- Inducto.Startup/Library.fs
+++ Inducto.Startup/Library.fs
@@ -12,3 +12,26 @@ let GetRandom() = Random().Next(10000, 99999).ToString()
 let GetGuid() = Guid.NewGuid()
 let someRandomStringList = [0..999] |> Seq.map(fun _ -> GetGuid())
 someRandomStringList |> Seq.iter(fun x -> printfn $"String: {x}") 
+let maper = someList|> List.map ( fun x -> x+1)
+let odd x = x%2=0
+let filtrer = someList|> List.filter odd 
+let plus x y = x+y
+let plusone = plus 1
+let itog = plusone 1
+
+let som1 = someList |> List.map plusone
+
+type Input = Input of string
+
+open System
+let input = ["a"; "3"; "f"; "55"]
+
+
+let toint1 = 
+    input 
+    |> List.map Int32.TryParse
+    |> List.filter (fun (x,y) -> x )
+    |> List.map snd
+    |> List.map (fun x -> x.ToString())
+
+
